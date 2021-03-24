4a6c1792061e8433cee73cc7f12e1c7dba8cdbc3
kaaneneskpc <kaaneneskpc1@gmail.com>
2021-03-24 13:34:05 +0300

    Refactoring Backend

---
+++ C#/ReCapProject/RecapProject/Core/Extensions/ErrorDetails.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace Core.Extensions
+{
+    public class ErrorDetails
+    {
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+
+        
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
